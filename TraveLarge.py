7ce3d908b0f9414aa5c57a0b1ca71a7c08147fe0
Raphael Lucas de Oliveira <raphael.onyx@gmail.com>
2016-04-21 15:38:45 -0300

    Update TraveLarge.py

--- TraveLarge.py
+++ TraveLarge.py
@@ -1,42 +1,49 @@
 class Noh:
+   def __init__(self,valor,pai=None):
+        self.valor=valor
+        self.pai=pai
+        self.filhos=[]
+        if pai is not None:
+            pai.filhos.append(self)
+            pai.filho_esquerdo=self
+        self.filho_esquerdo=None
+        self.irmao_direito=None
 
-    def __init__(self, valor, pai = None, filho_esquerdo = None, irmao_direito = None):
-        self.valor = valor
-        self.pai = pai
-        self.filho_esquerdo = filho_esquerdo
-        self.irmao_direito = irmao_direito
-
-        if pai != None:
-            pai.filho_esquerdo = self
-#nessa classe nao tem o que mudar
-
-    def adicionar(self, filho):
-
-
-        if self.filho_esquerdo:
-            proxFilho = self.filho_esquerdo
-            while proxFilho.irmao_direito:
-                proxFilho = proxFilho.irmao_direito
-            proxFilho.irmao_direito = filho
+   def adicionar(self,filho):
+        filho.pai=self
+        if len(self.filhos)==0:
+            self.filho_esquerdo=filho
         else:
-            filho.pai = self
-            self.filho_esquerdo = filho
-            filho.irmao_direito = None
-
+            filho2=self.filho_esquerdo
+            while filho2.irmao_direito is not None:
+                filho2=filho2.irmao_direito
+            filho2.irmao_direito=filho
+        self.filhos.append(filho)
 class Arvore:
-
-    def __init__(self, raiz = None):
-        self.raiz = raiz
+    def __init__(self,raiz=None):
+         self.raiz=raiz
 
     def altura(self):
-        altura_arvore = 0
-        while self.raiz != None:
-            self.raiz = self.raiz.filho_esquerdo
-            altura_arvore+=1
-        return altura_arvore
+        cont=0
+        ultimo=self.raiz
+        while ultimo != None: 
+            cont+=1
+            ultimo=ultimo.filho_esquerdo
+        return cont
 
+    def __iter__(self):
+        if self.raiz ==  None:
+             return self.raiz
+        noh_atual = []
+        noh_atual.append(self.raiz)
 
-        return 0
+        while noh_atual:
+            noh = noh_atual.pop(0)
+            if noh.pai == None:
+                yield noh.valor
+            for n in noh.filhos:
+                n.pai = None
+                noh_atual.append(n)
 
 
 
@@ -48,28 +55,26 @@ class NohTestes(TestCase):
         noh = Noh(5)
         self.assertEqual(5, noh.valor)
         self.assertIsNone(noh.pai)
-        self.assertIsNone(noh.irmao_direito)
-        self.assertIsNone(noh.filho_esquerdo)
+        self.assertListEqual([], noh.filhos)
 
     def teste_init_com_pai_justaposto(self):
         pai = Noh(5)
         filho = Noh(4, pai)
         self.assertIs(pai, filho.pai)
-        self.assertIs(filho, pai.filho_esquerdo)
+        self.assertIs(filho, pai.filhos[0])
 
     def teste_init_com_pai_nomeado(self):
         pai = Noh(5)
         filho = Noh(4, pai=pai)
         self.assertIs(pai, filho.pai)
-        self.assertIs(filho, pai.filho_esquerdo)
+        self.assertIs(filho, pai.filhos[0])
 
     def teste_adicionar_um_filho(self):
         pai = Noh(5)
         filho = Noh(4)
         pai.adicionar(filho)
         self.assertIs(pai, filho.pai)
-        self.assertIs(filho, pai.filho_esquerdo)
-        self.assertIsNone(filho.irmao_direito)
+        self.assertIs(filho, pai.filhos[0])
 
     def teste_adicionar_dois_filhos(self):
         pai = Noh(5)
@@ -78,9 +83,8 @@ class NohTestes(TestCase):
         pai.adicionar(filho)
         pai.adicionar(filho2)
         self.assertIs(pai, filho.pai)
-        self.assertIs(filho, pai.filho_esquerdo)
-        self.assertIs(filho2, filho.irmao_direito)
-        self.assertIsNone(filho2.irmao_direito)
+        self.assertIs(pai, filho2.pai)
+        self.assertListEqual([filho, filho2], pai.filhos)
 
     def teste_adicionar_tres_filhos(self):
         pai = Noh(5)
@@ -91,7 +95,53 @@ class NohTestes(TestCase):
         pai.adicionar(filho2)
         pai.adicionar(filho3)
         self.assertIs(pai, filho.pai)
-        self.assertIs(filho, pai.filho_esquerdo)
-        self.assertIs(filho2, filho.irmao_direito)
-        self.assertIs(filho3, filho2.irmao_direito)
-        self.assertIsNone(filho3.irmao_direito)
+        self.assertIs(pai, filho2.pai)
+        self.assertIs(pai, filho3.pai)
+        self.assertListEqual([filho, filho2, filho3], pai.filhos)
+
+
+class ArvoreTestes(TestCase):
+    def teste_init(self):
+        arvore = Arvore()
+        self.assertIsNone(arvore.raiz)
+
+    def teste_arvore_com_raiz(self):
+        noh = Noh(1)
+        arvore = Arvore(noh)
+        self.assertIs(noh, arvore.raiz)
+
+    def teste_altura_arvore(self):
+        self.assertEqual(0, Arvore().altura())
+        self.assertEqual(1, Arvore(Noh(1)).altura())
+        arvore_binaria = self.gerar_arvore_binaria()
+        self.assertEqual(4, arvore_binaria.altura())
+
+    def test_travesia_em_largura(self):
+        travessia_arvore_vazia = [i for i in Arvore()]
+        self.assertListEqual([], travessia_arvore_vazia)
+        travessia_arvore_unitaria = [i for i in Arvore(Noh(1))]
+        self.assertListEqual([1], travessia_arvore_unitaria)
+        arvore_binaria = self.gerar_arvore_binaria()
+        travessia_arvore_binaria = [i for i in arvore_binaria]
+        self.assertListEqual([5, 2, 8, 1, 4, 7, 9, 0, 3, 6], travessia_arvore_binaria)
+
+    def gerar_arvore_binaria(self):
+        nohs = [Noh(i) for i in range(10)]
+        raiz = nohs[5]
+        raiz.adicionar(nohs[2])
+        raiz.adicionar(nohs[8])
+
+        nohs[2].adicionar(nohs[1])
+        nohs[2].adicionar(nohs[4])
+
+        nohs[1].adicionar(nohs[0])
+
+        nohs[4].adicionar(nohs[3])
+
+        nohs[8].adicionar(nohs[7])
+        nohs[8].adicionar(nohs[9])
+
+        nohs[7].adicionar(nohs[6])
+
+        return Arvore(raiz)
+           
